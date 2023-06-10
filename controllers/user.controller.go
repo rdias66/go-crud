66d4d5076a6cc95825247128ce03620956ccc0d9
rdias66 <dias.a@live.com>
2023-06-10 15:17:58 -0300

    get route done

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -2,9 +2,12 @@ package users_controller
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"go-crud/models"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 )
 
@@ -12,5 +15,53 @@ func CreateUser(writer http.ResponseWriter, reader *http.Request, db *gorm.DB) {
 	writer.Header().Set("Content-Type", "application/json")
 	var user models.User
 	_ = json.NewDecoder(reader.Body).Decode(&user)
+	fmt.Println(reader.Body)
+	fmt.Println(user)
 	db.Create(&user)
+}
+
+func GetUser(writer http.ResponseWriter, reader *http.Request, db *gorm.DB) {
+	writer.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(reader)
+	userId := params["id"]
+
+	var userFound models.User
+
+	result := db.First(&userFound, userId)
+	if result.Error != nil { //Error handling
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			http.Error(writer, "User not found", http.StatusNotFound)
+		} else {
+			http.Error(writer, "Failed to fetch user", http.StatusInternalServerError)
+		}
+		return
+	}
+	writer.WriteHeader(http.StatusOK)
+
+	userJSON, err := json.Marshal(userFound)
+	if err != nil { // Error handling
+		http.Error(writer, "Failed to encode user", http.StatusInternalServerError)
+		return
+	}
+	writer.Write(userJSON)
+}
+
+func UpdateUser(writer http.ResponseWriter, reader *http.Request, db *gorm.DB) {
+	params := mux.Vars(reader)
+	userId := params["id"]
+	var userFound models.User
+	result := db.First(&userFound, userId)
+	if result.Error != nil { //Error handling
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			http.Error(writer, "User for update not found", http.StatusNotFound)
+		} else {
+			http.Error(writer, "Failed to fetch user", http.StatusInternalServerError)
+		}
+		return
+	}
+	var userUpdate models.User
+	_ = json.NewDecoder(reader.Body).Decode(&userUpdate)
+	fmt.Println(userFound)
+	userFound = userUpdate
+	fmt.Println(userUpdate)
 }
--- router/router.go
+++ router/router.go
@@ -10,8 +10,22 @@ import (
 
 func User_router(db *gorm.DB) http.Handler {
 	r := mux.NewRouter()
+
+	//CREATE USER
 	r.HandleFunc("/users/create", func(writer http.ResponseWriter, reader *http.Request) {
 		users_controller.CreateUser(writer, reader, db)
 	}).Methods("POST")
+
+	//GET USER
+	r.HandleFunc("/users/{id}", func(writer http.ResponseWriter, reader *http.Request) {
+		users_controller.GetUser(writer, reader, db)
+	}).Methods("GET")
+
+	/*//UPDATE USER
+	r.HandleFunc("/users/update", func(writer http.ResponseWriter, reader *http.Request) {
+		users_controller.UpdateUser(writer, reader, db)
+	}).Methods("PATCH")
+	*/
+
 	return r
 }

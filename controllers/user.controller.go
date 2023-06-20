9fd6c90f6855a6b1f78dd76d37f2d00334a37f36
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-06-20 08:29:05 -0300

    Update user.controller.go

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -46,22 +46,35 @@ func GetUser(writer http.ResponseWriter, reader *http.Request, db *gorm.DB) {
 	writer.Write(userJSON)
 }
 
-func UpdateUser(writer http.ResponseWriter, reader *http.Request, db *gorm.DB) {
+func UpdateUser(writer http.ResponseWriter, reader *http.Request, db *gorm.DB) { // need testing
+	writer.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(reader)
-	userId := params["id"]
-	var userFound models.User
-	result := db.First(&userFound, userId)
-	if result.Error != nil { //Error handling
-		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			http.Error(writer, "User for update not found", http.StatusNotFound)
+	userID := params["id"]
+
+	var existingUser models.User
+	if err := db.First(&existingUser, userID).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			http.Error(writer, "User not found", http.StatusNotFound)
 		} else {
 			http.Error(writer, "Failed to fetch user", http.StatusInternalServerError)
 		}
 		return
 	}
-	var userUpdate models.User
-	_ = json.NewDecoder(reader.Body).Decode(&userUpdate)
-	fmt.Println(userFound)
-	userFound = userUpdate
-	fmt.Println(userUpdate)
+
+	var updatedUser models.User
+	if err := json.NewDecoder(reader.Body).Decode(&updatedUser); err != nil {
+		http.Error(writer, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	existingUser.Name = updatedUser.Name
+	existingUser.Email = updatedUser.Email
+
+	if err := db.Save(&existingUser).Error; err != nil {
+		http.Error(writer, "Failed to update user", http.StatusInternalServerError)
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(writer).Encode(existingUser)
 }

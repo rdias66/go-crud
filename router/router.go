24a7c914a6aeaea9511e75975f9ddfb94b1b244a
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-06-20 08:29:56 -0300

    Update router.go

--- router/router.go
+++ router/router.go
@@ -21,11 +21,11 @@ func User_router(db *gorm.DB) http.Handler {
 		users_controller.GetUser(writer, reader, db)
 	}).Methods("GET")
 
-	/*//UPDATE USER
-	r.HandleFunc("/users/update", func(writer http.ResponseWriter, reader *http.Request) {
+	//UPDATE USER
+	r.HandleFunc("/users/{id}", func(writer http.ResponseWriter, reader *http.Request) {
 		users_controller.UpdateUser(writer, reader, db)
 	}).Methods("PATCH")
-	*/
+	
 
 	return r
 }
